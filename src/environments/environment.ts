23f786f624b1abad33df8f42e2a8578480e54e4f
troybedingfield <troybedingfield@gmail.com>
2025-10-06 23:19:01 -0700

    Stop tracking environment.localhost.ts

--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    supabaseUrl: 'https://kpukbvdsiwnshsjpbhqa.supabase.co',
-    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtwdWtidmRzaXduc2hzanBiaHFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgzNDY1NDIsImV4cCI6MjA3MzkyMjU0Mn0.H_eesOaFSNLwTKCuyNS2wmtH0HWomPM8e2c8WNRwJxg',
-}
