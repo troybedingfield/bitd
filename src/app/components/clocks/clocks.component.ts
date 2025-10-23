47f977ae4c996e7a66d379f4f190a5006bec606b
troybedingfield <troybedingfield@gmail.com>
2025-10-22 23:04:20 -0700

    update to tsconfig to es2024, esnext, and finshed advance and rewind clock functions and added select dropdown for clock number

--- src/app/components/clocks/clocks.component.ts
+++ src/app/components/clocks/clocks.component.ts
@@ -6,33 +6,47 @@ import { BarChart, PieChart } from 'echarts/charts';
 import { GridComponent } from 'echarts/components';
 import { CanvasRenderer } from 'echarts/renderers';
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { ButtonComponent } from "../../shared/components/button/button.component";
 echarts.use([BarChart, GridComponent, CanvasRenderer, PieChart]);
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-clocks',
-  imports: [NgxEchartsDirective], // Add NgxEchartsDirective to imports
+  imports: [NgxEchartsDirective, ButtonComponent, ReactiveFormsModule], // Add NgxEchartsDirective to imports
   providers: [provideEchartsCore({ echarts })],
   templateUrl: './clocks.component.html',
   styleUrl: './clocks.component.scss'
 })
 export class ClocksComponent implements OnInit {
 
+  // clocks = ['4', '6', '8', '10', '12', '14', '16', '20'];
+
+  clocks = [
+    { value: '4' },
+    { value: '6' },
+    { value: '8' },
+    { value: '10' },
+    { value: '12' },
+    { value: '14' },
+    { value: '16' },
+    { value: '20' },
+  ];
+
+  clockForm = new FormGroup({
+
+    clockLevels: new FormControl(null, Validators.required),
+
+
+
+
+  });
+
+
+
+
   @ViewChild(NgxEchartsDirective) myChart: any;
 
   chartOptions = {
-
-    title: {
-      // text: 'Referer of a Website',
-      // subtext: 'Fake Data',
-      // left: 'center'
-    },
-    tooltip: {
-      // trigger: 'item'
-    },
-    legend: {
-      // orient: 'vertical',
-      // left: 'left'
-    },
     series: [
       {
         // name: 'Access From',
@@ -57,23 +71,12 @@ export class ClocksComponent implements OnInit {
         labelLine: {
           show: false // Set to false to hide the label lines
         },
-        color: [
-          // '#ffffff',
-          // '#91cc75',
-          // '#91cc75',
-          // '#91cc75',
-          // '#91cc75',
-          // '#91cc75',
+        itemStyle: {
 
+          borderWidth: 3,
+          borderColor: '#000000'
+        }
 
-        ]
-        // emphasis: {
-        //   itemStyle: {
-        //     shadowBlur: 10,
-        //     shadowOffsetX: 0,
-        //     shadowColor: 'rgba(0, 0, 0, 0.5)'
-        //   }
-        // }
       }
     ]
 
@@ -86,65 +89,7 @@ export class ClocksComponent implements OnInit {
   }
 
   constructor() {
-    // this.chartOptions = {
 
-    //   title: {
-    //     // text: 'Referer of a Website',
-    //     // subtext: 'Fake Data',
-    //     // left: 'center'
-    //   },
-    //   tooltip: {
-    //     // trigger: 'item'
-    //   },
-    //   legend: {
-    //     // orient: 'vertical',
-    //     // left: 'left'
-    //   },
-    //   series: [
-    //     {
-    //       // name: 'Access From',
-    //       type: 'pie',
-    //       radius: '50%',
-    //       startAngle: 0,
-    //       decal: {
-    //         show: false
-    //       },
-    //       data: [
-    //         { value: 60, itemStyle: { color: 'white' } },
-    //         { value: 60, itemStyle: { color: 'white' } },
-    //         { value: 60, itemStyle: { color: 'white' } },
-    //         { value: 60, itemStyle: { color: 'white' } },
-    //         { value: 60, itemStyle: { color: 'white' } },
-    //         { value: 60, itemStyle: { color: 'white' } }
-    //       ],
-    //       label: {
-    //         show: false, // Set to false if you want to hide the labels completely
-    //         // ... other label configurations
-    //       },
-    //       labelLine: {
-    //         show: false // Set to false to hide the label lines
-    //       },
-    //       color: [
-    //         // '#ffffff',
-    //         // '#91cc75',
-    //         // '#91cc75',
-    //         // '#91cc75',
-    //         // '#91cc75',
-    //         // '#91cc75',
-
-
-    //       ]
-    //       // emphasis: {
-    //       //   itemStyle: {
-    //       //     shadowBlur: 10,
-    //       //     shadowOffsetX: 0,
-    //       //     shadowColor: 'rgba(0, 0, 0, 0.5)'
-    //       //   }
-    //       // }
-    //     }
-    //   ]
-
-    // };
   }
 
   advanceClock() {
@@ -180,6 +125,58 @@ export class ClocksComponent implements OnInit {
     // this.chartOptions.series[0].data = modifiedArray;
     this.chartOptions.series[0].data.splice(0, this.chartOptions.series[0].data.length, ...modifiedArray)
     this.myChart.setOption(this.chartOptions, true);
+  }
+  rewindClock() {
+    const targetValue = "green";
+    const newValueForFirstMatch = "white";
+
+    let firstMatchFound = false;
+
+    const originalArray = this.chartOptions.series[0].data
+
+    const modifiedArray = this.chartOptions.series[0].data.map((item: any, index: any) => {
+      console.log(item.itemStyle.color)
+      if (item.itemStyle.color === targetValue && !firstMatchFound) {
+        firstMatchFound = true; // Mark that the first match is found
+        return { ...item, itemStyle: { color: newValueForFirstMatch } }; // Create a new object with the updated value
+      }
+      return { ...item }; // Return a new object for other items to avoid modifying the original
+    });
+
+    console.log(modifiedArray)
+    console.log(this.chartOptions.series[0].data)
+
+    const firstElement = modifiedArray.shift();
+
+    // Add the stored first element to the end of the array
+    modifiedArray.push(firstElement);
+
+    this.chartOptions.series[0].data.splice(0, this.chartOptions.series[0].data.length, ...modifiedArray)
+    this.myChart.setOption(this.chartOptions, true);
+  }
+
+
+
+
+
+  async clockUpdate(postData: any) {
+
+    const clockLevels = postData.clockLevels.valueOf();
+
+
+
+    const formData = {
+      // char_id,
+
+      clockLevels,
+
+    }
+
+    console.log(formData);
+    // console.log(char_id);
+    // console.log(user);
+
+
   }
 
 }
