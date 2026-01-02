ac2d8356141460e173ad56920984ed09bcc346b2
troybedingfield <troybedingfield@gmail.com>
2026-01-01 20:56:46 -0800

    loading spinner added to character component

--- src/app/components/characters/characters.component.ts
+++ src/app/components/characters/characters.component.ts
@@ -2,17 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '@supabase/supabase-js';
 import { SupabaseService } from '../../supabase.service';
 import { CharacterCardComponent } from "../character-card/character-card.component";
-
+import { LoadingspinnerComponent } from '../../shared/components/loadingspinner/loadingspinner.component';
 
 @Component({
   selector: 'app-characters',
-  imports: [CharacterCardComponent],
+  imports: [CharacterCardComponent, LoadingspinnerComponent],
   templateUrl: './characters.component.html',
   styleUrl: './characters.component.scss'
 })
 export class CharactersComponent implements OnInit {
   items: any[] = [];
   currentUser: User | null = null;
+  loading = true;
 
   constructor(private supabaseService: SupabaseService) { }
 
@@ -21,6 +22,8 @@ export class CharactersComponent implements OnInit {
       this.items = await this.supabaseService.getItems('character');
     } catch (error) {
       console.error('Error fetching data:', error);
+    } finally {
+      this.loading = false;
     }
   }
 }
